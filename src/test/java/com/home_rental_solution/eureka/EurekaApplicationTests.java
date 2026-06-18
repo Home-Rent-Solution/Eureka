a8da82a88f8666ec024ac1e52ba567f0f4fff04f
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-06-18 11:27:13 -0400

    Se carga archivos iniciales

---
+++ src/test/java/com/home_rental_solution/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home_rental_solution.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
